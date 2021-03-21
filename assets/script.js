4d0ffe32db251660181f6f19eff9ef6e45c5b698
Seth Torek <seth.torek@gmail.com>
2021-03-21 15:58:41 -0400

    formatted html and css assigned variables in js

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+// variables ------------------------------------------------------------------
+var questionBox = document.getElementById("question-box")
+var answerBox = document.getElementById("answer-box")
+var mainBtn = document.getElementById("main-button")
+var highScore = document.getElementById("high-score")
+var time = document.getElementById("time-box")
+
