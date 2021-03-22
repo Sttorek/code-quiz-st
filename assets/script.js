3ca27dee384b1bb92f60fcc43d7314a0ff082397
Seth Torek <seth.torek@gmail.com>
2021-03-21 20:45:59 -0400

    in js created time interval function and message function

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,30 @@
 // variables ------------------------------------------------------------------
-var questionBox = document.getElementById("question-box")
-var answerBox = document.getElementById("answer-box")
-var mainBtn = document.getElementById("main-button")
-var highScore = document.getElementById("high-score")
-var time = document.getElementById("time-box")
+var questionBox = document.getElementById("question-box");
+var answerBox = document.getElementById("answer-box");
+var mainBtn = document.getElementById("main-button");
+var highScore = document.getElementById("high-score");
+var time = document.getElementById("time-box");
+var secondsLeft = 5;
 
+// --------------------------------------
+
+
+function sendMessage() {
+  document.getElementById("time").textContent = "Game Over!";
+  document.getElementById("time").setAttribute("style", "font-size: 40px;");
+  document.getElementById("time").setAttribute("style", "font-size: 40px;");
+//   !!!!!!!   how to set Multiple Data Attributes? !!!!!!!!!!!!!!!!!
+};
+
+
+mainBtn.addEventListener("click", function () {
+  var startTimer = setInterval(function () {
+    secondsLeft--;
+    time.textContent = secondsLeft;
+
+    if (secondsLeft === 0) {
+      clearInterval(startTimer);
+      sendMessage();
+    }
+  }, 1000);
+});
